490fef5f01d3e1c3351fb1ac826d1aee007c7c97
KimEunA97 <kimeuna9712@gmail.com>
2023-10-29 10:31:52 +0900

    라이브러리 설명 주석

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,9 +1,13 @@
 package Calculator;
 
-import javax.swing.JFrame;
+import javax.swing.*; // GUI 라이브러리
+import java.awt.*; //awt : Abstract Window Toolkit
+import java.awt.event.ActionListener; //이벤트 리스너
+import java.awt.event.ActionEvent; //이벤트 발생
+import java.util.ArrayList; //동적 배열 객체 관리 라이브러리
 
 public class Calculator extends JFrame {
-
+  // JFram : GUI 창 , JButton : 버튼
   public Calculator() {
   }
 }
